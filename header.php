0c4f4dfb69a0af3d4e1f174504752de6da11d21a
Fernando <fernandogiarola97@gmail.com>
2018-07-01 22:19:06 -0300

    PHP-UP

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
 	<head>
 		<title>PHP</title>
 		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="css/style.css"></link>
+		<link rel="stylesheet" type="text/css" href="style.css"></link>
 		
 		
 
